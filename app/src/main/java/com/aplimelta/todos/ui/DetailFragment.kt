7aca6e87376aa3f21ae0361797919fd2285f823e
Amiq Dian <66551148+amiqyolo@users.noreply.github.com>
2024-05-31 21:34:47 +0700

    show icon back on toolbar

--- app/src/main/java/com/aplimelta/todos/ui/DetailFragment.kt
+++ app/src/main/java/com/aplimelta/todos/ui/DetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.aplimelta.todos.data.response.ApiResponse
@@ -21,6 +22,11 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        (activity as AppCompatActivity).setSupportActionBar(binding?.toolbar)
+        (activity as AppCompatActivity).supportActionBar?.apply {
+            setDisplayShowHomeEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+        }
         return binding?.root
     }
 
